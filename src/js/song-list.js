ab577ea4a27068fc4e6ca1bd5f9797e1086c9b66
mdif <710740608@qq.com>
2019-01-29 15:42:16 +0800

    获取歌曲列表

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -5,31 +5,49 @@
     <ul class="songList" id="songList">
     </ul>
     `,
-    render(data){
-      
+    render(data) {
+
       $(this.el).html(this.template)
-      let {songs} = data
-      songs.map((song)=>{
+      let { songs } = data
+      songs.map((song) => {
         let li = $('<li></li>').text(song.name)
         $(this.el).find('#songList').append(li)
       })
     }
   }
   let model = {
-    data:{
-      songs:[]
-    }
-  }
-  let controler = {
-    init(view,model){
-      this.view = view
-      this.model = model
-      this.view.render(this.model.data)
-      window.eventHub.on('create',(data)=>{
-        this.model.data.songs.push(data)
-        this.view.render(this.model.data)
+    data: {
+      songs: []
+    },
+    find() {
+      var query = new AV.Query('song');
+      return query.find().then((songs)=> {
+        this.data.songs = songs.map((song) => {
+          return { id: song.id, ...song.attributes }
+        })
+        return songs
+      },(error)=>{
+        console.log(error)
       })
     }
   }
-  controler.init.call(controler,view,model)
+  let controler = {
+    init(view, model) {
+      this.view = view
+      this.model = model
+      this.view.render(this.model.data)
+      window.eventHub.on('create', (data) => {
+        this.model.data.songs.push(data)
+        this.view.render(this.model.data)
+      })
+      this.model.find().then((x) => {
+        console.log('x')
+        console.log(x)
+        this.view.render(this.model.data)
+      },(error)=>{
+        console.log(error)
+      })
+    }
+  }
+  controler.init.call(controler, view, model)
 }
