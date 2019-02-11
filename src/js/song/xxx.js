e479dd3642d846829ab91f6754d30917d4be2b91
mdif <710740608@qq.com>
2019-02-11 17:52:58 +0800

    加封面滚动和播放暂停按钮

--- src/js/song/xxx.js
+++ src/js/song/xxx.js
@@ -1,79 +1,106 @@
 {
   let view = {
-    el:'#playAudio',
-    template:`
-      <audio autoplay src="{{url}}"></audio>
-      <button id="play">播放</button>
-      <button id="pause">暂停</button>
-    `,
-    render(data){
-      console.log('render')
-      console.log(data.url)
-      $(this.el).html(this.template.replace('{{url}}',data.url))
+    el: '#playAudio',
+    render(data) {
+      let {song,status} = data
+      $(this.el).find('audio').attr('src',song.url)
+      if(status === 'playing'){
+        $(this.el).find('#cover').addClass('playing')
+      }else{
+        $(this.el).find('#cover').removeClass('playing')
+      }
     },
-    play(){
-      $(this.el).find('audio')[0].play()
+    play() {
+      console.log(12345)
+      let playPromise = $(this.el).find('audio')[0].play()
+      console.log($(this.el).find('audio')[0])
+      if(playPromise !== undefined){
+        playPromise.then(()=>{
+          console.log('-----')
+          $(this.el).find('audio')[0].play()
+          $(this.el).find('#icon-pause').addClass('active')
+          $(this.el).find('#icon-play').removeClass('active')
+          $(this.el).find('#cover').addClass('playing')
+        },(error)=>{
+          console.log(00000)
+          console.log(error)
+        })
+      }
     },
-    pause(){
+    pause() {
       $(this.el).find('audio')[0].pause()
+      $(this.el).find('#icon-play').addClass('active')
+      $(this.el).find('#icon-pause').removeClass('active')
+      $(this.el).find('#cover').removeClass('playing')
     }
-  } 
+  }
   let model = {
-    data:{
-      id:'',
-      name:'',
-      singer:'',
-      url:''
+    data: { 
+      song: {
+        id: '',
+        name: '',
+        singer: '',
+        url: ''
+      },
+      status:'status'
     },
-    setId(id){
+    setId(id) {
       this.data.id = id
     },
-    find(){
+    find() {
       var query = new AV.Query('song');
-      return query.get(this.data.id).then((song)=>{
-        Object.assign(this.data,{...song.attributes})
-        return song 
+      return query.get(this.data.id).then((song) => {
+        Object.assign(this.data.song, { ...song.attributes })
+        return song
       }, function (error) {
         console.log(error)
       })
+    },
+    setStatus(status){
+      this.data.status = status
     }
   }
   let controller = {
-    init(view,model){
+    init(view, model) {
       this.view = view
       this.model = model
-      this.bindEvents()
       this.model.setId(this.getId())
-      this.model.find().then((song)=>{
+      this.model.find().then((song) => {
         this.view.render(this.model.data)
-      },(error)=>{
+      }, (error) => {
         console.log(error)
       })
+      this.bindEvents()
     },
-    bindEvents(){
-      $(this.view.el).on('click','#play',()=>{
+    bindEvents() {
+      $(this.view.el).find('#icon-play').on('click',(e) => {
+        console.log('paly点了')
+        console.log($(this.view.el).find('#icon-play'))
         this.view.play()
+        this.model.setStatus('playing')
       })
-      $(this.view.el).on('click','#pause',()=>{
+      $(this.view.el).find('#icon-pause').on('click', () => {
+        console.log('pause被点了')
+        console.log($(this.view.el).find('#icon-pause'))
         this.view.pause()
-        console.log(111)
+        this.model.setStatus('pause')
       })
     },
-    getId(){
+    getId() {
       let search = window.location.search
-      if(search.indexOf('?') === 0){
+      if (search.indexOf('?') === 0) {
         search = search.substring(1)
       }
 
-      let array = search.split('&').filter(value=>value)
+      let array = search.split('&').filter(value => value)
       let id = ''
 
-      for(let i=0;i<array.length;i++){
+      for (let i = 0; i < array.length; i++) {
         let x = array[i]
         let newArray = x.split('=')
         let key = newArray[0]
         let value = newArray[1]
-        if(key === 'id'){
+        if (key === 'id') {
           id = value
           break
         }
@@ -81,5 +108,5 @@
       return id
     }
   }
-  controller.init.call(controller,view,model)
+  controller.init.call(controller, view, model)
 }
