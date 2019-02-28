c9f2303d6933b02cc69a4cfae3c98b9787dd994e
mdif <710740608@qq.com>
2019-02-28 13:22:06 +0800

    更新vendors

---
+++ vendors/js-sdk-1.0.2-beta/demo/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '<Your Access Key>',
+    'SECRET_KEY': '<Your Secret Key>',
+    'Bucket_Name': '<Your Bucket Name>',
+    'Port': 18080,
+    'Uptoken_Url': '/uptoken',
+    'Domain': 'http://qiniu-plupload.qiniudn.com/'
+};
